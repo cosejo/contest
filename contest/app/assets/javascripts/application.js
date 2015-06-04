2b2085313c21a885a1e02da3fda7c4a3e4b78176
Carlos Osejo <caosejo@gmail.com>
2015-06-04 00:25:34 -0600

    App background universe

--- contest/app/assets/javascripts/application.js
+++ contest/app/assets/javascripts/application.js
@@ -14,3 +14,22 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(window).load(function(){
+
+  var body = $("body"),
+      universe = $("#universe"),
+      solarsys = $("#solar-system");
+
+  var init = function() {
+    body.removeClass('view-2D opening').addClass("view-3D").delay(2000).queue(function() {
+      $(this).removeClass('hide-UI').addClass("set-speed");
+      $(this).dequeue();
+    });
+  };
+
+  var setView = function(view) { universe.removeClass().addClass(view); };
+
+  init();
+
+});
