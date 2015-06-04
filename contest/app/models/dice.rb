1511bc2e67ff02d9fd65296f695e667b3b7e45f2
Carlos Osejo <caosejo@gmail.com>
2015-06-03 23:39:34 -0600

    Throw with only enable game types

--- contest/app/models/dice.rb
+++ contest/app/models/dice.rb
@@ -2,12 +2,18 @@ class Dice < ActiveRecord::Base
 	attr_accessible  :last_value, :range
 
 	def throw
-		throw_value = rand(1..range)
+		game_types = get_available_game_types
+		self.range = game_types.count - 1
+		throw_value = game_types[rand(0..range)].id
 		while throw_value == last_value
-			throw_value = rand(1..range)
+			throw_value = game_types[rand(0..range)].id
 		end
 		self.last_value = throw_value
 		self.save
 		throw_value
+	end
+
+	def get_available_game_types
+		GameType.joins(:games).where(games: {disabled:false}).order(:id).uniq
 	end
 end
