88c19d2436cd6a7a88c021d5e58477f4dbbc8ae9
Carlos Osejo <caosejo@gmail.com>
2015-06-22 22:18:03 -0600

    End game

--- contest/app/controllers/game_controller.rb
+++ contest/app/controllers/game_controller.rb
@@ -3,5 +3,12 @@ class GameController < ApplicationController
 	def index
 		@game = Game.find(params[:id])
 	end
-	
+
+	def end
+		game = Game.find(params[:id])
+		game.disabled = true
+		game.save
+		redirect_to dice_url()
+	end
+
 end
